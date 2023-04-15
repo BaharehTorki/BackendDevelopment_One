30632d9542fda6bf9358e16d728b5dfd1c4300dc
BaharehTorki <bahareh.torki@yh.nackademin.se>
2023-04-15 18:59:11 +0200

    Adding nicknames, birthdays and list of numbers and adress to persons

--- src/main/java/com/example/spring_assignments/telefonboken/FriendsDAO.java
+++ src/main/java/com/example/spring_assignments/telefonboken/FriendsDAO.java
@@ -8,11 +8,47 @@ public class FriendsDAO {
 
         List<FriendsPOJO> friendsList = new ArrayList<>();
 
-        FriendsPOJO f1 = new FriendsPOJO(1, "Simon Edlund", "0731211234");
-        FriendsPOJO f2 = new FriendsPOJO(2, "Danielle Johansson", "07328790578");
-        FriendsPOJO f3 = new FriendsPOJO(3, "Cristian Gustavsson", "07376894056");
-        FriendsPOJO f4 = new FriendsPOJO(4, "Emilia Davidsson", "0732342678");
-        FriendsPOJO f5 = new FriendsPOJO(5, "Viliam Björklund", "0733456345");
+        List<String> phones1 = new ArrayList<>();
+        phones1.add("0731211234");
+        phones1.add("0738789767");
+        List<String> adress1 = new ArrayList<>();
+        adress1.add("Solnavägen4");
+
+        List<String> phones2 = new ArrayList<>();
+        phones2.add("0731245456");
+        phones2.add("0732349879");
+        List<String> adress2 = new ArrayList<>();
+        adress2.add("Frösundavägen5");
+        adress2.add("Kolonvägen6");
+        adress2.add("Råsundavägen7");
+
+        List<String> phones3 = new ArrayList<>();
+        phones3.add("0738712343");
+        phones3.add("0731767876");
+        phones3.add("0732767865");
+        List<String> adress3 = new ArrayList<>();
+        adress3.add("Rissneleden45");
+        adress3.add("Samuelsgatan56");
+
+        List<String> phones4 = new ArrayList<>();
+        phones4.add("0731744545");
+        List<String> adress4 = new ArrayList<>();
+        adress4.add("Barkarbyvägen45");
+
+        List<String> phones5 = new ArrayList<>();
+        phones5.add("0731744545");
+        phones5.add("0732234323");
+        phones5.add("0732234323");
+        List<String> adress5 = new ArrayList<>();
+        adress5.add("Månadsvägen14");
+        adress5.add("Sunbybargvägen14");
+        adress5.add("Veckovägen44");
+
+        FriendsPOJO f1 = new FriendsPOJO(1, "Simon Edlund", "Victor", phones1, "2011-05-23", adress1);
+        FriendsPOJO f2 = new FriendsPOJO(2, "Danielle Johansson", "Sofia", phones2, "2008-04-16", adress2 );
+        FriendsPOJO f3 = new FriendsPOJO(3, "Cristian Gustavsson", "Izabel", phones3,"2000-01-10", adress3);
+        FriendsPOJO f4 = new FriendsPOJO(4, "Emilia Davidsson" ,"Izabel", phones4, "1990-04-22", adress4 );
+        FriendsPOJO f5 = new FriendsPOJO(5, "Viliam Björklund", "Samuel", phones5, "1790-05-11", adress5);
 
         friendsList.add(f1);
         friendsList.add(f2);
@@ -22,5 +58,4 @@ public class FriendsDAO {
 
         return friendsList;
     }
-
 }
--- src/main/java/com/example/spring_assignments/telefonboken/FriendsPOJO.java
+++ src/main/java/com/example/spring_assignments/telefonboken/FriendsPOJO.java
@@ -4,11 +4,16 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class FriendsPOJO {
     private int id;
     private String name;
-    private String phoneNumber;
+    private String nickName;
+    private List<String> phoneNumbers;
+    private String birthday;
+    private List<String> otherAdress;
 
 }
